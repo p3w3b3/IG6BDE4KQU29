cf306555c26acb82e6b87daaa5bcf3bb790b963c
p3w3b3 <110833128+p3w3b3@users.noreply.github.com>
2022-08-08 23:28:11 +0900

    Update and rename FB.js to FBs.js

--- TR/MX/FBs.js
+++ TR/MX/FBs.js
@@ -19,7 +19,6 @@
     measurementId: "G-EZSWDWDTB6"
   };
   
-  mixpanel.init('34ab50f7acd413779598bfe06a25b8c3', {debug: true}); 
 
       $(".section1selection").css("background-color", "white");
       $(".section1div").hide();
@@ -107,7 +106,7 @@
   {
   (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/e84ncf6y';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
   }
-        
+  mixpanel.init('34ab50f7acd413779598bfe06a25b8c3', {debug: true});   
        
   
       let m1p1 = data['m1p1'];    
