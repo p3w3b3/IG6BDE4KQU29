a297dfb25c9600bcd77e6d7bf3c2cfb2a2cd5157
p3w3b3 <110833128+p3w3b3@users.noreply.github.com>
2022-08-10 17:18:38 +0900

    Update and rename THREE-1.js to THREE-11.js

--- OP/THREE-11.js
+++ OP/THREE-11.js
@@ -172,7 +172,7 @@ window.intercomSettings = {
 (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/e84ncf6y';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 }
 
-
+let sted = data['stid']
 
           // if gsign and no name
           if (!!name) {
@@ -185,7 +185,7 @@ window.intercomSettings = {
               .set({ Name: user.displayName }, { merge: true });
             {
               setTimeout(() => {
-                window.location.href = "/enterprise/all";
+                window.location.href = "/enterprise/" + sted;
               }, 2000);
             }
             $("#userName2").html(name);
@@ -195,8 +195,8 @@ window.intercomSettings = {
 
 
 			  
-          if (currentPath !== "/enterprise/all") {
-            location.href = "/enterprise/all";
+          if (currentPath !== "/enterprise/" + sted) {
+            location.href = "/enterprise/" + sted;
           }
 
           // if #actions
