38eaf7d6aa71cb0d4a4910134efb49485d1ba26e
p3w3b3 <110833128+p3w3b3@users.noreply.github.com>
2022-08-15 17:31:39 +0900

    Update and rename VM.js to VMs.js

--- TR/AB/VMs.js
+++ TR/AB/VMs.js
@@ -1,53 +1,53 @@
 setTimeout(() => { 
 {
-  document.getElementById("s1p1button").addEventListener("click", (event) => {
+  document.getElementById("sm1p1button").addEventListener("click", (event) => {
     $(".section1selection").css("background-color", "white");
     $(".section1div").hide();
-    $("#s1p1video").show();
-    $("#s1p1button").css("background-color", "#e7edf0");
+    $("#sm1p1video").show();
+    $("#sm1p1button").css("background-color", "#e7edf0");
   });
 }
 
 {
-  document.getElementById("s1p2button").addEventListener("click", (event) => {
+  document.getElementById("sm1p2button").addEventListener("click", (event) => {
     $(".section1selection").css("background-color", "white");
     $(".section1div").hide();
-    $("#s1p2video").show();
-    $("#s1p2button").css("background-color", "#e7edf0");
+    $("#sm1p2video").show();
+    $("#sm1p2button").css("background-color", "#e7edf0");
   });
 }
 
 {
-  document.getElementById("s1p3button").addEventListener("click", (event) => {
+  document.getElementById("sm1p3button").addEventListener("click", (event) => {
     $(".section1selection").css("background-color", "white");
     $(".section1div").hide();
-    $("#s1p3video").show();
-    $("#s1p3button").css("background-color", "#e7edf0");
+    $("#sm1p3video").show();
+    $("#sm1p3button").css("background-color", "#e7edf0");
   });
 }
 
 
 const checkcompleted1 = function() {
 let prognum = 0;
-    if (document.querySelector("#s1p1x").style.display == "block") {
+    if (document.querySelector("#sm1p1x").style.display == "block") {
       prognum++;
     }
-    if (document.querySelector("#s1p2x").style.display == "block") {
+    if (document.querySelector("#sm1p2x").style.display == "block") {
       prognum++;
     }
-    if (document.querySelector("#s1p3x").style.display == "block") {
+    if (document.querySelector("#sm1p3x").style.display == "block") {
       prognum++;
     }
     document.querySelector("#prognum").textContent = prognum;
 }
 
 {
-  var iframe = document.querySelector("#s1p1video");
+  var iframe = document.querySelector("#sm1p1video");
   var player = new Vimeo.Player(iframe);
   var finished = function () {
     videoended = "true";
-    $("#s1p1").hide();
-    $("#s1p1x").show();
+    $("#sm1p1").hide();
+    $("#sm1p1x").show();
     firebase
       .firestore()
       .doc("users/" + firebase.auth().currentUser.uid)
@@ -58,12 +58,12 @@ let prognum = 0;
 }
 
 {
-  var iframe = document.querySelector("#s1p2video");
+  var iframe = document.querySelector("#sm1p2video");
   var player = new Vimeo.Player(iframe);
   var finished = function () {
     videoended = "true";
-    $("#s1p2").hide();
-    $("#s1p2x").show();
+    $("#sm1p2").hide();
+    $("#sm1p2x").show();
     firebase
       .firestore()
       .doc("users/" + firebase.auth().currentUser.uid)
@@ -74,12 +74,12 @@ let prognum = 0;
 }
 
 {
-  var iframe = document.querySelector("#s1p3video");
+  var iframe = document.querySelector("#sm1p3video");
   var player = new Vimeo.Player(iframe);
   var finished = function () {
     videoended = "true";
-    $("#s1p3").hide();
-    $("#s1p3x").show();
+    $("#sm1p3").hide();
+    $("#sm1p3x").show();
     firebase
       .firestore()
       .doc("users/" + firebase.auth().currentUser.uid)
