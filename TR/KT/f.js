2cdc074499ec029288e6472a90f922f3398c1060
p3w3b3 <110833128+p3w3b3@users.noreply.github.com>
2022-09-09 14:03:32 +0900

    Update and rename FBB.js to f.js

--- TR/KT/f.js
+++ TR/KT/f.js
@@ -70,8 +70,6 @@ document.getElementById("retake-k6").addEventListener("click", () => {
     auth.onAuthStateChanged(async (user) => {
       let currentPath = window.location.pathname;
       if (user) {
-        maincontent.style.display='flex';
-            loader.style.display='none';
         let myFS = firebase.firestore()
         let docSnap = await myFS.doc("users/"+firebase.auth().currentUser.uid).get();
         let data = docSnap.data()
