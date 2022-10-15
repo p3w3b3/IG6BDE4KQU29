d901c75aa1e48d63fa517aaebf1bbe815b32e78c
p3w3b3 <110833128+p3w3b3@users.noreply.github.com>
2022-10-15 14:29:16 +0300

    Update and rename f.js to fb.js

--- TR/KT/fb.js
+++ TR/KT/fb.js
@@ -139,12 +139,7 @@ document.querySelector('#languageselector').textContent = lang
       let profileuid = curUser.uid     
 let sstid = data['stid'].toLowerCase()
 let ops = data['operations']
-document.querySelector("#mydas").addEventListener("click", () => {
-window.location.href = '/operations/'+sstid
-})
-document.querySelector("#mbmenu1").addEventListener("click", () => {
-window.location.href = '/operations/'+sstid
-})
+
 if(ops == true){
 $("#operations").css("display", "flex");
 $('#normal').hide()
@@ -606,10 +601,6 @@ document.querySelector('#profile-restaurant').textContent=restaurant;
     firebase.firestore().doc("users/"+firebase.auth().currentUser.uid)
       .set({KTOWN:spn}, {merge:true})
     
-    
-     if(prereq !== true) {
-            location.href = '/onboarding';
-            }
      if(!!name) {
           $('#userName2').html(name);
       } else {
