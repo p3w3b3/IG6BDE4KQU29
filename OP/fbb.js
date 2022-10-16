bf12ae61c1f6c2d0130d19d88b3d6103d4cbaf5d
p3w3b3 <110833128+p3w3b3@users.noreply.github.com>
2022-10-16 23:32:10 +0300

    Update and rename fbase.js to fbb.js

--- OP/fbb.js
+++ OP/fbb.js
@@ -46,14 +46,15 @@ let usc = data["uscontent"];
 
 let stid = data['stid'].toUpperCase()
 let myselect = document.querySelector('#selectstid')
-let cusd = document.getElementById('username-'+stid).textContent
+
 
 if(stid.includes(',')){
   let mys = stid.split(',')
   for (i = 0 ; i < mys.length; i++) {
+  let cus = document.getElementById('username-'+mys[i]).textContent
   myselect.add(new Option(cus,mys[i]));
   }} else {
-  let cus = document.getElementById('username-'+mys[i]).textContent
+let cusd = document.getElementById('username-'+stid).textContent
   myselect.add(new Option(cusd,stid));
   }
 
