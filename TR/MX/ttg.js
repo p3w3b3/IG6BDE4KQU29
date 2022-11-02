2a2a054eb383487e4e1538c9783b29da3190a56d
p3w3b3 <110833128+p3w3b3@users.noreply.github.com>
2022-11-03 02:53:21 +0700

    Update and rename STGG.js to ttg.js

--- TR/MX/ttg.js
+++ TR/MX/ttg.js
@@ -66,7 +66,7 @@ const firebaseConfig = {
         window.intercomSettings = {
             api_base: "https://api-iam.intercom.io",
             app_id: "e84ncf6y",
-            hide_default_launcher: true,
+            hide_default_launcher: false,
             name: "'"+name+"'", // Full name
             email: "'"+emaild+"'", // Email address
             user_id: "'"+profileuid+"'" // UID FIREBASE
@@ -86,6 +86,20 @@ const firebaseConfig = {
   document.querySelector('#profile-uid').textContent=profileuid;
   document.querySelector('#profile-name').textContent=name;
   document.querySelector('#userName3').textContent=name;
+
+document.querySelector('#userName').textContent=name;
+document.querySelector('#userName2').textContent=name;
+
+if(emaild.length>30) {
+document.querySelector('#email').textContent = emaild.slice(0,17) + '...'
+document.querySelector('#email2').textContent = emaild.slice(0,17) + '...'
+} else {
+document.querySelector('#email').textContent=emaild;
+document.querySelector('#email2').textContent=emaild;
+}
+
+
+
   document.querySelector('#profile-email').textContent=emaild;
   if(!!restaurant) {
   document.querySelector('#profile-restaurant').textContent=restaurant;
@@ -542,7 +556,6 @@ $("#trackermob").hide();
     }
   
     if(!!emaild) {
-        $('#email').html(emaild);
     }} else {
       console.log('user logged out');
       signOutButton.style.display="none";
