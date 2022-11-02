dcf0829b65da82bb7837b74a45f888b437b243aa
p3w3b3 <110833128+p3w3b3@users.noreply.github.com>
2022-11-03 02:52:44 +0700

    Update and rename STGG.js to ttg.js

--- TR/PT/ttg.js
+++ TR/PT/ttg.js
@@ -65,10 +65,10 @@ mixpanel.init('bb6d2603674e27d7b31d045acf3ae77f', {debug: true});
 window.intercomSettings = {
     api_base: "https://api-iam.intercom.io",
     app_id: "e84ncf6y",
-    hide_default_launcher: true,
+    hide_default_launcher: false,
     name: "'"+name+"'", // Full name
     email: "'"+emaild+"'", // Email address
-		user_id: "'"+profileuid+"'" // UID FIREBASE
+    user_id: "'"+profileuid+"'" // UID FIREBASE
   };
 }
 {
@@ -342,6 +342,19 @@ document.querySelector('#profile-uid').textContent=profileuid;
 document.querySelector('#profile-name').textContent=name;
 document.querySelector('#userName3').textContent=name;
 document.querySelector('#profile-email').textContent=emaild;
+	      
+document.querySelector('#userName').textContent=name;
+document.querySelector('#userName2').textContent=name;
+
+if(emaild.length>30) {
+document.querySelector('#email').textContent = emaild.slice(0,17) + '...'
+document.querySelector('#email2').textContent = emaild.slice(0,17) + '...'
+} else {
+document.querySelector('#email').textContent=emaild;
+document.querySelector('#email2').textContent=emaild;
+}
+
+	      
 if(!!restaurant) {
 document.querySelector('#profile-restaurant').textContent=restaurant;
 } else {document.querySelector('#profile-restaurant').textContent='-';} 
@@ -547,7 +560,6 @@ document.querySelector('#profile-restaurant').textContent=restaurant;
         }
 
         if (!!emaild) {
-          $("#email").html(emaild);
         }
       } else {
         console.log("user logged out");
