d6ba80fd96d82b4ea23041806677c0bef0692e09
p3w3b3 <110833128+p3w3b3@users.noreply.github.com>
2022-11-03 02:55:49 +0700

    Update and rename STGG.js to ttg.js

--- TR/RR/ttg.js
+++ TR/RR/ttg.js
@@ -95,6 +95,19 @@ signOutButton2.addEventListener('click', signout2);
 document.querySelector('#profile-uid').textContent=profileuid;
 document.querySelector('#profile-name').textContent=name;
 document.querySelector('#userName3').textContent=name;
+
+document.querySelector('#userName').textContent=name;
+document.querySelector('#userName2').textContent=name;
+
+if(emaild.length>30) {
+document.querySelector('#email').textContent = emaild.slice(0,17) + '...'
+document.querySelector('#email2').textContent = emaild.slice(0,17) + '...'
+} else {
+document.querySelector('#email').textContent=emaild;
+document.querySelector('#email2').textContent=emaild;
+}
+
+
 document.querySelector('#profile-email').textContent=emaild;
 if(!!restaurant) {
 document.querySelector('#profile-restaurant').textContent=restaurant;
@@ -106,7 +119,7 @@ document.querySelector('#profile-restaurant').textContent=restaurant;
 window.intercomSettings = {
     api_base: "https://api-iam.intercom.io",
     app_id: "e84ncf6y",
-    hide_default_launcher: true,
+    hide_default_launcher: false,
     name: "'"+name+"'", // Full name
     email: "'"+emaild+"'", // Email address
 		user_id: "'"+profileuid+"'" // UID FIREBASE
@@ -545,7 +558,6 @@ $('#mbcbox').hide()
         }
 
         if (!!emaild) {
-          $("#email").html(emaild);
         }
       } else {
         console.log("user logged out");
