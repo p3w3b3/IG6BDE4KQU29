121a41be35c33682dcf9cd69e2beef8482809636
p3w3b3 <110833128+p3w3b3@users.noreply.github.com>
2023-03-28 17:23:32 +0300

    Update and rename porq.js to porqo.js

--- P/porqo.js
+++ P/porqo.js
@@ -183,6 +183,21 @@
         }
         
         let profileuids = curUser.uid
+
+
+        if (window.innerWidth < 1024){
+        {
+        window.intercomSettings = {
+        api_base: "https://api-iam.intercom.io",
+        app_id: "e84ncf6y",
+        hide_default_launcher: true,
+        name: name, // Full name
+        email: emaild, // Email address
+        user_id: profileuids, // UID FIREBASE
+        custom_launcher_selector:'#intercom-url',
+
+        }}
+        } else {
         {
         window.intercomSettings = {
         api_base: "https://api-iam.intercom.io",
@@ -193,8 +208,13 @@
         user_id: profileuids, // UID FIREBASE
         custom_launcher_selector:'#intercom-url',
 
-        };
+        }}
+
+
         }
+
+
+
         {
         
         (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/e84ncf6y';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
