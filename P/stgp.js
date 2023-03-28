c6a09a20aca532f3a04cce154025982752c87179
p3w3b3 <110833128+p3w3b3@users.noreply.github.com>
2023-03-28 17:47:14 +0300

    Update and rename stonp.js to stgp.js

--- P/stgp.js
+++ P/stgp.js
@@ -57,17 +57,35 @@ location.href = 'https://stonegate.peckwaterbrands.com/operations/'+stid.toLower
     }
     
     let profileuids = curUser.uid
-    {
-    window.intercomSettings = {
-    api_base: "https://api-iam.intercom.io",
-    app_id: "e84ncf6y",
-    hide_default_launcher: false,
-    name: name, // Full name
-    email: emaild, // Email address
-    user_id: profileuids, // UID FIREBASE
-    custom_launcher_selector:'#intercom-url'
-    };
-    }
+
+
+    if (window.innerWidth < 1024){
+        {
+        window.intercomSettings = {
+        api_base: "https://api-iam.intercom.io",
+        app_id: "e84ncf6y",
+        hide_default_launcher: true,
+        name: name, // Full name
+        email: emaild, // Email address
+        user_id: profileuids, // UID FIREBASE
+        custom_launcher_selector:'#intercom-url',
+
+        }}
+        } else {
+        {
+        window.intercomSettings = {
+        api_base: "https://api-iam.intercom.io",
+        app_id: "e84ncf6y",
+        hide_default_launcher: false,
+        name: name, // Full name
+        email: emaild, // Email address
+        user_id: profileuids, // UID FIREBASE
+        custom_launcher_selector:'#intercom-url',
+
+        }}
+
+
+        }
     {
     
     (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/e84ncf6y';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
